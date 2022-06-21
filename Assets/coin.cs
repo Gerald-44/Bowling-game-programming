36104aedd0838b202b5cec55796c6dbc78dee971
matador19 <alexmaina1997@gmail.com>
2022-06-21 11:23:36 +0300

    Updated files

---
+++ Assets/coin.cs
@@ -0,0 +1,18 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    
+       [SerializeField] 
+       private float spinSpeed =10f;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+       transform.Rotate(0f, spinSpeed, 0f ,Space.World); 
+    }
+}
