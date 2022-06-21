126213d4f7e8d92037f29096e390ef3bb1bd4b3c
matador19 <alexmaina1997@gmail.com>
2022-06-21 11:58:17 +0300

    ball movement and camera movement

---
+++ Assets/ballMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+   public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        //Allows player to move horizontally
+        float moveH=Input.GetAxis("Horizontal");
+
+        //Allows player to move vertically
+        float moveV=Input.GetAxis("Vertical");
+
+        Vector3 movement=new Vector3(moveH,0.0f,moveV);
+
+        rb.AddForce(movement*speed);
+    }
+}
